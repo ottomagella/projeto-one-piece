7981c2d9588f786d6bd209af492a4c8a2646742a
ottomagella <contato.otaviomagella@outlook.com>
2023-10-01 01:17:17 -0300

    Adicionando arquivo js

---
+++ src/js/index.js
@@ -0,0 +1,42 @@
+/* 
+    O que precisamos fazer? - quando clicar no botão do personagem na lista temos que marcar o botão como selecionado e mostrar o personagem correspondente.
+
+    OBJETIVO 1 - quando clicar no botão do personagem na lista, marcar o botao como selecionado .
+        passo 1 - pegar os botões no JS pra poder verificar quando o usuário clicar em cima de um deles;
+        passo 2 - adicionar a classe "selecionado" no botão que o usuário clicou;
+        passo 3 - verificar se já existe um botão selecionado, se sim, devemos remover a seleção dele.
+
+    OBJETIVO 2 - quando clicar no botão do personagem mostrar as informações do personagem.
+        passo 1 - pegar os personagens no JS pra poder mostrar ou esconder ele;
+        passo 2 - adicionar a classe "selecionado" no personagem que o usuário selecionou;
+        passo 3 - verificar se já exista um personagem selecionado, se sim, devemos remover a seleção dele.
+*/
+
+// Quando clicar no botão do personagem na lista, marcar o botao como selecionado
+
+// Pegar os botões no JS pra poder verificar quando o usuário clicar em cima de um deles
+const botoes = document.querySelectorAll('.botao')
+
+// Quando clicar no botão do personagem mostrar as informações do personagem
+
+// Pegar os personagens no JS pra poder mostrar ou esconder ele
+const personagens = document.querySelectorAll(".personagem")
+
+botoes.forEach((botao, indice) => {
+    botao.addEventListener("click", () => {
+
+        // Verificar se já existe um botão selecionado, se sim, devemos remover a seleção dele
+        const botaoSelecionado = document.querySelector(".botao.selecionado")
+        botaoSelecionado.classList.remove("selecionado")
+
+        // Adicionar a classe "selecionado" no botão que o usuário clicou
+        botao.classList.add("selecionado")
+
+        // Verificar se já exista um personagem selecionado, se sim, devemos remover a seleção dele
+        const personagemSelecionado = document.querySelector(".personagem.selecionado")
+        personagemSelecionado.classList.remove("selecionado")
+
+        // Adicionar a classe "selecionado" no personagem que o usuário selecionou
+        personagens[indice].classList.add("selecionado")
+    })
+})
